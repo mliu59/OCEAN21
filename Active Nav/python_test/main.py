8901fc4b6fa748974a016254a63d0dd16e65f89f
Miles Liu <mliu59@jhu.edu>
2021-02-24 19:00:38 -0500

    added final GUI and command deque changes

--- Active Nav/python_test/main.py
+++ Active Nav/python_test/main.py
@@ -8,44 +8,58 @@ import time
 from mavsdk import System
 from mavsdk.follow_me import (Config, FollowMeError, TargetLocation)
 
-GUI_ITEM_WIDTH = 40
+testCommands = ["arm",
+                "takeoff",
+                "hold,5",
+                "goto,47.39803985,8.54557254",
+                "goto,47.39803622,8.54501464",
+                "goto,47.39782562,8.54500928",
+                "hold,5",
+                "goto,47.39803985,8.54557254",
+                "goto,47.39803622,8.54501464",
+                "goto,47.39782562,8.54500928",
+                "hold,5",
+                "land",
+                "disarm"]
+TEST = True
+
+BASE_ACTION_DELAY = 10
+TIME_PER_COORD = 20
+GUI_ITEM_WIDTH = 50
 COMMAND_DEQUE_MAX_DISPLAY = 10
 DATALOG_PERIOD = 5
 DATALOG_OUTPUT_MODE = "w" #for overwrite, or "a" for append
 DATALOG_FILENAME = "datalog.csv"
-COMMANDS = ["arm",
+COMMANDS = ["arm",   #commands tied to processCommand function
             "takeoff",
             "land",
             "disarm",
             "kill",
             "goto", #followed with args:set target coords
-            "startfollow",#start follow mode
-            "followtarget", #followed with args:set follow target
-            "stopfollow", #stop follow mode
-            "hold"] #followed with args: hold time
+            "hold",
+            "startfollow",
+            "followtarget",
+            "stopfollow"] #followed with args: hold time
 DRONE_INFO_TAGS = ["Battery",
                    "Flight_Mode",
                    "Drone_Lat",
                    "Drone_Lon",
                    "Drone_Alt",
                    "GPS_Fix",
-                   "Drone_Last_Updated",
-                   "Origin_Lat",
-                   "Origin_Lon",
-                   "Origin_Alt"]
+                   "Drone_POS_Last_Updated"]
+                   #"Origin_Lat",
+                   #"Origin_Lon",
+                   #"Origin_Alt"]
 ROV_INFO_TAGS =   ["ROV_Lat",
                    "ROV_Lon",
                    "ROV_Depth",
-                   "ROV_Last_Updated"]
+                   "ROV_POS_Last_Updated"]
 INFO_UPDATE_PERIOD = 1
 DEFAULT_HEIGHT = 8.0 #in Meters
 FOLLOW_DISTANCE = 2.0 #in Meters, this is the distance that the drone will remain away from Target while following it 
 FOLLOW_DIRECTION = Config.FollowDirection.BEHIND #NONE, FRONT, FRONT_LEFT, FRONT_RIGHT, BEHIND
 FOLLOW_RESPONSIVENESS = 0.02
-
-"""
-fake_location = [[47.398039859999997,8.5455725400000002],[47.398036222362471,8.5450146439425509],[47.397825620791885,8.5450092830163271]]
-"""
+DEQUE_RESPOND_PERIOD = 1
 
 class info:
     class topic:
@@ -95,7 +109,10 @@ class info:
     
 class commandDeque: #addBottom(input), #addTop
     def __init__(self, root, c):
-        self.command = []
+        if TEST:
+            self.command = testCommands
+        else: 
+            self.command = []
         self.meta = [tk.StringVar(), tk.StringVar()]
         self.maxDisplay = COMMAND_DEQUE_MAX_DISPLAY
         self.displayedCommands = []
@@ -160,7 +177,7 @@ class commandDeque: #addBottom(input), #addTop
                      
 def initGUI():
     window = tk.Tk(className='-- Info GUI --')
-    window.geometry("700x700")
+    window.geometry("700x600")
     return window
 def initInfoPanels(array, title, window, c):
     textLabel(window, title, c, -1)
@@ -182,10 +199,87 @@ def textVarLabel(window, var, c, r):
 def got_stdin_data(q):
     asyncio.ensure_future(q.put(sys.stdin.readline().replace('\n', '')))
 
-async def processCommand(command):
-    ##TODO##
-    #print(command)
-    await asyncio.sleep(0.1)
+async def processCommand(drone, deque):
+    while 1:
+        data = None
+        if deque.count() != 0:
+            command = deque.popTop()
+            args = command.split(",")
+            if args[0] == COMMANDS[0]:
+                try:    
+                    await drone.action.arm()
+                    await asyncio.sleep(BASE_ACTION_DELAY)
+                except: pass
+            elif args[0] == COMMANDS[1]:
+                try:    
+                    await drone.action.takeoff()
+                    await asyncio.sleep(BASE_ACTION_DELAY)
+                except: pass
+            elif args[0] == COMMANDS[2]:
+                try:    
+                    await drone.action.land()
+                    await asyncio.sleep(BASE_ACTION_DELAY)
+                except: pass
+            elif args[0] == COMMANDS[3]:
+                try:    
+                    await drone.action.disarm()
+                    await asyncio.sleep(BASE_ACTION_DELAY)
+                except: pass
+            elif args[0] == COMMANDS[4]:
+                try:    
+                    await drone.action.kill()
+                except: pass
+            elif args[0] == COMMANDS[5]: #goto
+                try:
+                    lat = float(args[1])
+                    lon = float(args[2])
+                    async for terrain_info in drone.telemetry.home():
+                        absolute_altitude = terrain_info.absolute_altitude_m
+                        break
+                    await drone.action.goto_location(lat, lon, absolute_altitude + DEFAULT_HEIGHT, 0)
+                    await asyncio.sleep(TIME_PER_COORD)
+                except: 
+                    print(f"Bad Request: {args}")
+                    pass
+
+            elif args[0] == COMMANDS[6]: #hold
+                try:    
+                    await asyncio.sleep(float(args[1]))
+                except:
+                    print(f"Bad Request: {args}")
+                    pass
+            elif args[0] == COMMANDS[7]: #start follow
+                try:    
+                    conf = Config(DEFAULT_HEIGHT, FOLLOW_DISTANCE, FOLLOW_DIRECTION, FOLLOW_RESPONSIVENESS)
+                    await drone.follow_me.set_config(conf)
+                    await drone.follow_me.start()
+                except: 
+                    print(f"Bad Request: {args}")
+                    pass
+            elif args[0] == COMMANDS[8]: #follow target
+                try:
+                    target = TargetLocation(float(args[1]), float(args[2]), 0, 0, 0, 0)
+                    await drone.follow_me.set_target_location(target)
+                except: 
+                    print(f"Bad Request: {args}")
+                    pass
+            elif args[0] == COMMANDS[9]: #stop follow
+                try:    
+                    await drone.follow_me.stop()
+                except: pass
+            elif args[0] == "quit":
+                print("-- quitting --")
+                break
+            else:
+                print(f"?: {args}")
+                await asyncio.sleep(10)
+            
+        else:
+            deque.clearCurrent()
+            await asyncio.sleep(DEQUE_RESPOND_PERIOD)
+        
+        #print(command)
+        #await asyncio.sleep(0.1)
         
 async def tcp_echo_client(loop): #network client
     reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
@@ -205,8 +299,8 @@ def update_drone_info(drone, info):
     b = asyncio.ensure_future(gpsfixAsync(drone, info))
     c = asyncio.ensure_future(posAsync(drone, info))
     d = asyncio.ensure_future(flightmodeAsync(drone, info))
-    e = asyncio.ensure_future(originAsync(drone, info))
-    return [a, b, c, d, e]
+    #e = asyncio.ensure_future(originAsync(drone, info))
+    return [a, b, c, d]
     
 async def batteryAsync(drone, info):
     async for battery in drone.telemetry.battery():
@@ -221,17 +315,20 @@ async def posAsync(drone, info):
         info.updateVal("Drone_Alt", round(position.relative_altitude_m))
         info.updateVal("Drone_Lat", position.latitude_deg)
         info.updateVal("Drone_Lon", position.longitude_deg)
+        info.updateVal("Drone_POS_Last_Updated", time.time())
     await asyncio.sleep(INFO_UPDATE_PERIOD)      
 async def flightmodeAsync(drone, info):    
     async for flight_mode in drone.telemetry.flight_mode():
         info.updateVal("Flight_Mode", flight_mode)
     await asyncio.sleep(INFO_UPDATE_PERIOD)
+"""
 async def originAsync(drone, info):    
     async for origin in drone.telemetry.GpsGlobalOrigin():
         info.updateVal("Origin_Lat", origin.latitude_deg)
         info.updateVal("Origin_Lon", origin.longitude_deg)
         info.updateVal("Origin_Alt", origin.altitude_m)
-    await asyncio.sleep(INFO_UPDATE_PERIOD * 50)
+    await asyncio.sleep(INFO_UPDATE_PERIOD)
+"""
     
 async def parseCommand(command, q, interval=0.1):
     """
@@ -246,10 +343,11 @@ async def parseCommand(command, q, interval=0.1):
         if args[0][0] == '!':
             if args[0][1:] in COMMANDS:
                 command.addTop(data[1:])
+            else: print("Illegal Command\n")
         else:
             if args[0] in COMMANDS:
                 command.addBottom(data)
-        
+            else: print("Illegal Command\n")
         await asyncio.sleep(interval)
 
 async def outputToDataLog(infos, command, filename, mod):
@@ -276,7 +374,7 @@ async def updateGUI(command, root, interval=0.1):
         root.update()
         await asyncio.sleep(interval)
 
-async def main_loop(asyncQueue):    
+async def main_loop(asyncQueue, loop):    
     
     drone = System()
     await drone.connect(system_address="udp://:14540")
@@ -292,34 +390,28 @@ async def main_loop(asyncQueue):
         if health.is_global_position_ok:
             print("Global position estimate OK")
             break
-
+    
+    print("Initializing GUI & Command Deque...")
+    
     window = initGUI()
     dInfo = initInfoPanels(DRONE_INFO_TAGS, '-- Drone Health + Positional Info --', window, 0)
     textLabel(window, "\n---------------\n", 0, -1)
     rInfo = initInfoPanels(ROV_INFO_TAGS, '-- ROV Info --', window, 0)
     command = commandDeque(window, 1)
     
+    print("Drone ready for commands")
+    
     transfer_task = asyncio.ensure_future(parseCommand(command, asyncQueue))
     GUI_task = asyncio.ensure_future(updateGUI(command, window))
     dataLogging_task = asyncio.ensure_future(outputToDataLog([dInfo, rInfo], command, DATALOG_FILENAME, DATALOG_OUTPUT_MODE))
-    #print_health_task = asyncio.ensure_future(update_drone_info(drone, dInfo))
     updateInfoTasks = update_drone_info(drone, dInfo)
     #print_coords_task = asyncio.ensure_future(tcp_echo_client(asyncio.get_event_loop()))
     
-    while 1:
-        data = None
-        if command.count() != 0:
-            data = command.popTop()
-        else:
-            command.clearCurrent()
-        if data == "quit":
-            print("----- quitting -----")
-            break
-        asyncio.ensure_future(processCommand(data))
-        await asyncio.sleep(5)
+    mainTask = asyncio.ensure_future(processCommand(drone, command))
+    await mainTask
 
 if __name__ == "__main__":
     q = asyncio.Queue()
     event_loop = asyncio.get_event_loop()
     event_loop.add_reader(sys.stdin, got_stdin_data, q)
-    event_loop.run_until_complete(main_loop(q))
+    event_loop.run_until_complete(main_loop(q, event_loop))
