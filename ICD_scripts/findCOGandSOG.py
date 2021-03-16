d9a6149266d56d5dce7431e3d29cb670418de400
Mohid169 <mohidkhan169@gmail.com>
2021-03-16 17:43:51 -0400

    Add files via upload

---
+++ ICD_scripts/findCOGandSOG.py
@@ -0,0 +1,25 @@
+import numpy as np
+import math as math
+import geopy 
+from geographiclib.geodesic import Geodesic ... 
+
+def findDistance(rovLat1,rovLong1,depth1, rovLat2,rovLong2,depth2):
+	coord1=(rovLat1,rovLong1) 
+	coord2=(rovLat2,rovLong2) 
+	d=geopy.distance.vincenty(coord1,coord2).m
+	print(d)
+	d_2=pow(d,2)
+	h=(depth1-depth2)
+	h_2=pow(h,2)
+	print(math.sqrt(d_2+h_2))
+	return math.sqrt(d_2+h_2)
+	
+
+def findCOG(rovLat1,rovLong1,rovLat2,rovLong2):
+	 COG = Geodesic.WGS84.Inverse(lat1, long1, lat2, long2)['azi1']  #find the bearing of the path w/ respect to truth north (azimuth)
+	 return COG
+def findSOG(rovLat1,rovLong1,rovLat2,rovLong2,Te): #TE is the time elapsed between the two sample points
+	D=findDistance(rovLat1,rovLong1,rovLat2,rovLong2)
+	SOG=D/Te
+	return SOG #Rate equation Distance/time elapsed S0G
+
