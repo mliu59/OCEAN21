828b0bba4cb8e02c7669ab5c205d0d14f01f229f
Mohid169 <mohidkhan169@gmail.com>
2021-04-19 03:14:46 -0400

    Update Psuedodatalogging.py

--- PsuedoROV code/Psuedodatalogging.py
+++ PsuedoROV code/Psuedodatalogging.py
@@ -109,7 +109,7 @@ while True:
 	vehiclePos=getROVCoords(ROV)
 	vehicleRPY=rov.rpy
 	vehicleSpeed=rov.velocity
-	dataString=print(str(vehiclePos[0])+","+str(vehiclePos[1])+","+str(0)+","+str(0)+","+str(0)+","+str(vehicleRPY[2])+","+str(vehicleSpeed[0])+","+str(vehicleSpeed[1])+","+str(0)+","+"*")
+	dataString=str(vehiclePos[0])+","+str(vehiclePos[1])+","+str(0)+","+str(0)+","+str(0)+","+str(vehicleRPY[2])+","+str(vehicleSpeed[0])+","+str(vehicleSpeed[1])+","+str(0)+","+"*"
 	#print()
 	with open("ROVdata.csv", "a")as output:
         writer = csv.writer(output, delimiter=",")
